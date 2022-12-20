11d28c067ce5c221aa6be09f7528820ba8172bc2
Habibaeo <63856368+Habibaeo@users.noreply.github.com>
2022-12-20 20:44:24 +0500

    Add files via upload

---
+++ Atm-Machine/index.js
@@ -0,0 +1,166 @@
+#! /usr/bin/env node
+import chalk from "chalk";
+import inquirer from "inquirer";
+import chalkAnimation from "chalk-animation";
+const duration = () => {
+    return new Promise((res) => {
+        setTimeout(res, 1000);
+    });
+};
+async function start() {
+    let firstNote = chalkAnimation.rainbow(`
+   #    ####### #     #       #     #    #     #####  #     # ### #     # #######
+  # #      #    ##   ##       ##   ##   # #   #     # #     #  #  ##    # #
+ #   #     #    # # # #       # # # #  #   #  #       #     #  #  # #   # #
+#     #    #    #  #  # ##### #  #  # #     # #       #######  #  #  #  # #####
+#######    #    #     #       #     # ####### #       #     #  #  #   # # #
+#     #    #    #     #       #     # #     # #     # #     #  #  #    ## #
+#     #    #    #     #       #     # #     #  #####  #     # ### #     # ####### \n\n`);
+    await duration();
+    firstNote.stop();
+}
+let balance = Math.floor(Math.random() * 1000000);
+async function userSet() {
+    await start();
+    var login = await inquirer.prompt([
+        {
+            type: "input",
+            name: "setUser",
+            message: chalk.yellow("Set user name : "),
+            hide: true,
+        },
+        {
+            type: "password",
+            name: "pin",
+            message: chalk.yellowBright("Set your 4 digit PIN : "),
+            mask: "*",
+            validate: (input) => {
+                let stringNumber = JSON.stringify(input);
+                if (isNaN(input) ||
+                    stringNumber.length - 2 > 4 ||
+                    stringNumber.length - 2 < 4) {
+                    return chalk.red("Pin must be a number and four digits long");
+                }
+                else {
+                    return true;
+                }
+            },
+        }
+    ]);
+    return login.setUser;
+}
+async function options() {
+    let operation = await inquirer.prompt([
+        {
+            type: "list",
+            name: "actions",
+            message: chalk.yellow("Choose the desired operation"),
+            choices: ["Check Balance", "Withdraw", "Transfer"],
+        },
+    ]);
+    if (operation.actions == "Check Balance") {
+        console.log(chalk.cyan(`  Your available Balance is : ${balance}$`));
+    }
+    else if (operation.actions == "Withdraw") {
+        console.log(chalk.green(`Available Balance = $${balance}`));
+        let withdraw = await inquirer.prompt([
+            {
+                type: "input",
+                name: "withdrawl",
+                message: chalk.cyan("Enter withdrawl amount : $"),
+                validate: (inputNumber) => {
+                    if (isNaN(inputNumber) || inputNumber > balance) {
+                        return chalk.red("Not a Number or Greater than Available balance");
+                    }
+                    else {
+                        return true;
+                    }
+                },
+            },
+            {
+                type: "confirm",
+                name: "receipt",
+                message: chalk.yellow("Do you want to print receipt"),
+            },
+        ]);
+        if (withdraw.withdrawl <= balance && withdraw.receipt) {
+            console.log(chalk.green(`        Withdrawl Amount : ${withdraw.withdrawl}$ 
+        Remaining Balance : ${balance - withdraw.withdrawl}$
+        Transaction Successful`));
+            balance = balance - withdraw.withdrawl;
+        }
+        else if (withdraw.withdrawl <= balance) {
+            console.log(chalk.green("Transaction Successful"));
+        }
+    }
+    else if (operation.actions == "Transfer") {
+        console.log(chalk.green(`Available Balance = $${balance}`));
+        let transfer = await inquirer.prompt([
+            {
+                type: "input",
+                name: "account",
+                message: chalk.cyan("Enter receiver's account number : "),
+                validate: (inputNumber1) => {
+                    if (isNaN(inputNumber1)) {
+                        return chalk.red("Not a valid Account Number");
+                    }
+                    else {
+                        return true;
+                    }
+                },
+            },
+            {
+                type: "input",
+                name: "transferAmount",
+                message: chalk.cyan("Enter Amount to Transfer : $"),
+                validate: (inputNumber2) => {
+                    if (isNaN(inputNumber2) || inputNumber2 > balance) {
+                        return chalk.red("Not a Number or Greater than Available balance");
+                    }
+                    else {
+                        return true;
+                    }
+                },
+            },
+            {
+                type: "confirm",
+                name: "proceed",
+                message: chalk.yellow("Do you want to proceed"),
+            },
+        ]);
+        if (transfer.transferAmount <= balance && transfer.proceed) {
+            let receiveReceipt = await inquirer.prompt({
+                type: "confirm",
+                name: "receipt",
+                message: chalk.yellow("Do you want to print receipt"),
+            });
+            if (receiveReceipt.receipt) {
+                console.log(chalk.green(`              Beneficiary Account : ${transfer.account}  
+              Transfer Amount : ${transfer.transferAmount}$ 
+              Remaining Balance : ${balance - transfer.transferAmount}$
+              Transaction Successful`));
+                balance = balance - transfer.transferAmount;
+            }
+            else {
+                console.log(chalk.green("Transaction successful"));
+            }
+        }
+        else {
+            console.log(chalk.red("    Transaction Aborted"));
+        }
+    }
+}
+async function repeat() {
+    do {
+        await options();
+        var ask = await inquirer.prompt([
+            {
+                type: "confirm",
+                name: "doagain",
+                message: chalk.yellow("To perform another operation press Y/N"),
+            },
+        ]);
+    } while (ask.doagain);
+}
+await userSet();
+repeat();
